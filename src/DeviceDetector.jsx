684850ca412771ed0b41fd31d767eb6a35b43156
kyriosaa <kyriosaaph@gmail.com>
2024-11-04 02:25:35 +0800

    Mobile test change

--- src/DeviceDetector.jsx
+++ src/DeviceDetector.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import Background from "./Background";
 
 const DeviceDetector = ({ children }) => {
   const [isMobile, setIsMobile] = useState(false);
@@ -18,29 +18,7 @@ const DeviceDetector = ({ children }) => {
 
   if (isMobile) {
     console.log("Mobile device detected"); // Debugging information
-    return (
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-        textAlign="center"
-        bgcolor="black"
-        color="white"
-        padding={2}
-        zIndex={9999} // Ensure the box is on top
-        position="fixed" // Ensure the box covers the entire screen
-        top={0}
-        left={0}
-        width="100%"
-      >
-        <Typography variant="h6">
-          This website is only available on PC. Please visit from a desktop or
-          laptop computer.
-        </Typography>
-      </Box>
-    );
+    return <Background />;
   }
 
   return children;
