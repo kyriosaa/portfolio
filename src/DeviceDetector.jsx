971235d736f06ebb9f4879dbf83c998a3703323c
kyriosaa <kyriosaaph@gmail.com>
2024-11-04 02:52:33 +0800

    Update HTML

--- src/DeviceDetector.jsx
+++ src/DeviceDetector.jsx
@@ -6,7 +6,6 @@ const DeviceDetector = ({ children }) => {
 
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    console.log("User Agent:", userAgent); // Debugging information
     if (
       /android/i.test(userAgent) ||
       (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)
@@ -16,7 +15,6 @@ const DeviceDetector = ({ children }) => {
   }, []);
 
   if (isMobile) {
-    console.log("Mobile device detected"); // Debugging information
     return (
       <p>
         This website is only available on PC. Please visit from a desktop or
