0a85bb3ca8d25ed3c35315a4aed25649e420d5cd
kyriosaa <kyriosaaph@gmail.com>
2024-11-07 10:46:00 +0800

    Update from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Background from "./Background";
 import Home from "./pages/home";
 import Projects from "./pages/projects";
