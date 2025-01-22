6524250a85121e7473b9bf528f832a7ac90e7d9d
kyriosaa <kyriosaaph@gmail.com>
2025-01-23 00:42:40 +0700

    Update graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,10 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
       postsRemark: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+        filter: {
+          fileAbsolutePath: { regex: "/content/posts/" }
+          frontmatter: { slug: { ne: "/pensieve/" } }
+        }
         sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
