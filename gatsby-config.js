8793a2b3b3ab563fc4a56804f72193cfa0899bd7
kyriosaa <kyriosaaph@gmail.com>
2025-01-23 01:59:10 +0700

    Add new media images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Purit Hongjirakul',
-    description:
-      'Purit Hongjirakul is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Purit Hongjirakul website portfolio.',
     siteUrl: 'https://purithongjirakul.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
