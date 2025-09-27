03e7b85a6ac7f2815962ea2b7717e1f0054ab968
kyriosaa <kyriosaaph@gmail.com>
2025-09-27 14:55:29 +0800

    added github workflows

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     image: '/ph.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
+  pathPrefix: "/portfolio",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
